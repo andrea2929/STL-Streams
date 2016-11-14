d97bd6a2738b993693c7c49dd73db29b628603bd
andrea2929 <halenkampa1@nku.edu>
2016-11-14 17:21:45 -0500

    Managed to get the file to read in properly and take in the strings.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,74 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
+struct Patient {
+    string fname;
+    string mname;
+    string lname;
+    string suffix;
+    vector<string> ailments;
+    string doctor;
+    bool treated;
+    int priority;
+};
+
+vector<Patient> readfile(string filename) {
 
     ifstream myfile;
-    myfile.open("/Users/andreahalenkamp/ClionProjects/Project3/patients.txt");
+    myfile.open(filename);
 
     string line;
 
-    while ( getline(myfile,line,',') )
+    vector<Patient> patients(3);
+    Patient current;
+    int n = 0;
+
+    while ( getline(myfile,line) )
     {
-        cout << line << endl;
+
+        if (!line.empty()) {
+            if (!line.substr(0,line.find(":")).compare("firstName"))
+                current.fname = line.substr(line.find(":") + 1);
+            if (!line.substr(0,line.find(":")).compare("middleName"))
+                current.mname = line.substr(line.find(":") + 1);
+            if (!line.substr(0,line.find(":")).compare("lastName"))
+                current.lname = line.substr(line.find(":") + 1);
+            if (!line.substr(0,line.find(":")).compare("suffix"))
+                current.suffix = line.substr(line.find(":") + 1);
+            if (!line.substr(0,line.find(":")).compare("doctor"))
+                current.doctor = line.substr(line.find(":") + 1);
+        }
+        else {
+            patients[n] = current;
+            n++;
+        }
     }
+
+    patients[n] = current;
+
+    return patients;
+}
+
+void printPatients(vector<Patient> patients) {
+
+    for(int i = 0; i < patients.size(); i++) {
+        cout << patients[i].fname
+             << " "
+             << patients[i].mname
+             << " "
+             << patients[i].lname
+             << " goes to doctor "
+             << patients[i].doctor
+             << endl;
+    }
+}
+
+int main() {
+    vector<Patient> p = readfile("/Users/andreahalenkamp/ClionProjects/Project3/patients.txt");
+
+    printPatients(p);
 }
