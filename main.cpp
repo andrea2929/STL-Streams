ad3cd525c8ff6133d923efd226c9c98db4f532a6
andrea2929 <halenkampa1@nku.edu>
2016-11-14 15:59:06 -0500

    Program now reads the file line by line.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    //hello world
+
     ifstream myfile;
     myfile.open("/Users/andreahalenkamp/ClionProjects/Project3/patients.txt");
 
@@ -12,6 +12,6 @@ int main() {
 
     while ( getline(myfile,line,',') )
     {
-
+        cout << line << endl;
     }
 }
